ce05ea5f9fa6ab83623db15daf8ed1db25d95ba5
kuzma <kuzma89klg@gmail.com>
2023-12-21 08:45:31 +0300

    Linear search is added.

---
+++ algorhitms/search/linearSearch/linearSearch.ts
@@ -0,0 +1,22 @@
+import Comparator, { CompareType } from '../../../utils/Comparator';
+
+export default function linearSearch<T>(input: T[], target: T, compareCallback?: CompareType): number[] {
+  const comparator = new Comparator(compareCallback);
+  const match = [];
+
+  input.forEach((item, index) => {
+    if (comparator.equal(item, target)) {
+      match.push(index);
+    }
+  });
+
+  return match;
+}
+
+/**
+ * Time complexity:
+ *    Access  O(n)
+ *    Search  O(n)
+ * Insertion  O(n)
+ *  Deletion  O(n)
+ */
